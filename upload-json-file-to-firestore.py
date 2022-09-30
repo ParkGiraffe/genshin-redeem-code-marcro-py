0f94f8fc27e74685cb5ca2954fa3f6a5f5c6930d
ParkGiraffe <op5321@soongsil.ac.kr>
2022-09-30 19:25:50 +0900

    Feat: Firestore에 json 파일 업로드 성공

--- upload-json-file-to-firestore.py
+++ upload-json-file-to-firestore.py
@@ -2,15 +2,15 @@ from firebase_admin import credentials, firestore, initialize_app
 import json
 
 cred = credentials.Certificate('serviceAccountKey.json')
-initialize_app(cred)
+default_app = initialize_app(cred)
 
 class UploadJsonFileToFireStore:
     def __init__(self):
-        self._json_file = 'data.json'
-        self._firestore_collection = 'collection'
+        self._json_file = 'validRedeemCodes.json'
+        self._firestore_collection = 'redeem_code_list'
         self._firestore_document = 'document'
         self._firestore_field = 'field'
-        self._firestore = firestore.Client()
+        self._firestore = firestore.Client.from_service_account_json('serviceAccountKey.json')
         self._json_data = self._get_json_data()
         self._upload_json_data_to_firestore()
 
@@ -22,3 +22,5 @@ class UploadJsonFileToFireStore:
         self._firestore.collection(self._firestore_collection).document(self._firestore_document).set({
             self._firestore_field: self._json_data
         })
+
+UploadJsonFileToFireStore()
